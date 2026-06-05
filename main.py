1ef4667cb9b1c6a77ab52a3b92396944c426124d
ssannssarr <ssantanusarkar8@gmail.com>
2026-06-05 17:32:36 +0530

    added timesleep

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from ui.rui import cp,pnl,main_panel,prompt,c,reply,think
-
+from ui.rui import cp,pnl,main_panel,prompt,c,reply,think,end
+import time
 c.clear()
 
 main_panel()
 
 usr_in=""
 try:
-
-while True:
-    usr_in = prompt("openai/Iris")
-    if usr_in != "/exit":
-        think(f"you typed {usr_in}")
-        reply("iris","* hiii")
-    else:
-        break
+    while True:
+        usr_in = prompt("openai/Iris")
+        if usr_in != "/exit":
+            think(f"you typed {usr_in}")
+            time.sleep(3)
+            reply("iris","* hiii")
+        else:
+            break
+except KeyboardInterrupt:
+    end()
