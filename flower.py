471165a24c467adcd9e7ba046b0933e8bae4f06b
ssannssarr <ssantanusarkar8@gmail.com>
2026-06-05 12:14:48 +0530

    added hardcoded gradient to flower

--- flower.py
+++ flower.py
@@ -1,3 +1,6 @@
+from rich.text import Text
+from ui.rui import cp
+
 f= """
 
 
@@ -13,3 +16,34 @@ f= """
             ≋≋∿∿      ⌇~~~
 
 """
+
+COLOR_MAP = {
+    "~": "#581c87",
+    "∿": "#6b21a8",
+    "≋": "#7e22ce",
+    "∼": "#9333ea",
+    "⌇": "#a855f7",
+
+    "◦": "#c084fc",
+    "∘": "#d8b4fe",
+    "○": "#e9d5ff",
+    "·": "#c4b5fd",
+
+    "✿": "#f5e8ff",
+    " ": None,
+}
+
+
+def paint_flower(FLOWER):
+    txt = Text()
+
+    for ch in FLOWER:
+        if ch == "\n":
+            txt.append("\n")
+        else:
+            color = COLOR_MAP.get(ch, "#ffffff")
+            txt.append(ch, style=color)
+
+    return txt
+
+cp(paint_flower(f))
